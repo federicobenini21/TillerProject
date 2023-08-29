0057a6b0c9578bddfcaca4ab34b2529a7b44baf8
Federico Benini <federicobenini21@gmail.com>
2023-08-29 13:19:59 +0000

    orders-table-clean

---
+++ models/orders.sql
@@ -0,0 +1,13 @@
+SELECT
+    id_order
+    ,id_store
+    ,date_opened
+    ,date_closed
+    ,m_nb_customer AS number_customers
+    ,m_cached_payed AS total_payed
+    ,m_cached_price AS total_price
+    ,id_table
+    ,id_waiter
+    ,id_device
+FROM `TillerProject.order_data`
+WHERE m_cached_payed > 0 AND m_cached_price > 0
