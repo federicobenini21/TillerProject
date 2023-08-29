3bf3cff97b8eeef0e47ed773372d11db2045e66c
Federico Benini <federicobenini21@gmail.com>
2023-08-29 13:32:57 +0000

    order-lines-clean

---
+++ models/orders_lines.sql
@@ -0,0 +1,16 @@
+SELECT
+    id_order_line
+    ,id_order
+    ,date_opended AS date_opened
+    ,date_created
+    ,m_quantity AS qty
+    ,m_unit_price AS unit_price
+    ,m_unit_price_exc_vat AS unit_price_exc_vat
+    ,m_total_price_inc_vat AS tot_price_inc_vat
+    ,m_total_price_exc_vat AS tot_price_exc_vat
+    ,m_tax_percent AS tax_percent
+    ,m_discount_amount AS discount_amount
+    ,dim_type AS line_type
+    ,dim_category AS line_category
+    ,dim_name AS line_name
+FROM `TillerProject.order_line`
